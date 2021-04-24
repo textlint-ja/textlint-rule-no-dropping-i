8573f558e69900f5623789ad49da5e55bd08add0
azu <azuciao@gmail.com>
2021-04-24 22:02:05 +0900

    refactor: use ===

--- src/no-dropping-i.js
+++ src/no-dropping-i.js
@@ -4,22 +4,22 @@ import { RuleHelper } from "textlint-rule-helper";
 import { tokenize } from "kuromojin";
 
 function isTargetWord(token) {
-    return token.pos == "助詞" && token.pos_detail_1 == "接続助詞" && token.basic_form == "て";
+    return token.pos === "助詞" && token.pos_detail_1 === "接続助詞" && token.basic_form === "て";
 }
 
 function isMasuWord(token) {
-    return token.pos == "助動詞" && token.pos_detail_1 == "*" && token.basic_form == "ます";
+    return token.pos === "助動詞" && token.pos_detail_1 === "*" && token.basic_form === "ます";
 }
 
 module.exports = function(context) {
     const helper = new RuleHelper(context);
-    let { Syntax, report, getSource, RuleError } = context;
+    const { Syntax, report, getSource, RuleError } = context;
     return {
         [Syntax.Str](node) {
             if (helper.isChildNode(node, [Syntax.Link, Syntax.Image, Syntax.BlockQuote, Syntax.Emphasis])) {
                 return;
             }
-            let text = getSource(node);
+            const text = getSource(node);
             return tokenize(text).then(tokens => {
                 tokens.reduce((prev, current) => {
                     if (isTargetWord(prev) && isMasuWord(current)) {
